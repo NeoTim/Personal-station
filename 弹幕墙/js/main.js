1ce077ec462314c169173c18b091be390c21a9bf
我的头发呢 <44613792+jxqys@users.noreply.github.com>
2018-11-24 11:59:44 +0800

    弹幕墙完善

--- 弹幕墙/js/main.js
+++ 弹幕墙/js/main.js
@@ -1,54 +1,69 @@
 $(function(){
   var ref=new Wilddog("https://wd6545407118xbblrl.wilddogio.com/");
-
-  var timer=[];
+  // var timer=[];
+  var arr=[];
   $('.submit1').click(function(){
     // console.log(1);
      var speak=$('#speak').val();
     // console.log(speak);
     ref.child('message').push(speak);
     $('#speak').val('');
-    move();
   });
-    var danmu;
-    ref.child('message').on('child_added', function(snapshot) {
-	    var txt = snapshot.val();
-	    danmu = $("<div class='danmu'></div>");
-	    danmu.text(txt);
+  $("#speak").keypress(function(event) {
+      if (event.keyCode == "13") {
+        $(".submit1").trigger('click');
+        event.preventDefault();              //阻止默认行为
+      }
+    });
+
+  ref.child('message').on('child_added', function(snapshot) {
+	    var text = snapshot.val();
+      arr.push(text);
+	    var danmu = $("<div class='danmu'></div>");
+	    danmu.text(text);
 	    $(".jumbotron").append(danmu);
-      danmu.css({
-                position:'absolute',
-                fontSize:'20px',
-                display:'block'
-            });
-      var col = '#' + ((Math.random() * 0x1000000 << 0).toString(16));
-      danmu.css({
-                color: col,
-                top: (Math.floor(Math.random() * $('.jumbotron').height())-24) + "px",
-                width:danmu.width(),
-                right: 0
-            });
+      move(danmu);
 
 	  });//点击事件
 
-    $('.submit2').click(function(){
+  $('.submit2').click(function(){
       ref.remove();
+      arr=[];
+      // $('.jumbotron').empty();
     });
-    ref.on('child_removed', function() {
-        $('.danmu').remove();
+  ref.on('child_removed', function() {
+        // $('.danmu').remove();
+        arr = [];
+	    $('.jumbotron').empty();
       });
 
-    function move(){
-        var i = 0;
-        var timer = setInterval(function() {
-            danmu.css({
-                right: (i += 1) + "px"
+  var move=function(obj){
+      var col = '#' + ((Math.random() * 0x1000000 << 0).toString(16));
+      obj.css({
+                color: col,
+                top: (Math.floor(Math.random() * $('.jumbotron').height())) + "px",
+                width:500,
+                right: -500,
+                display:'block',
+                position:'absolute',
+                fontSize:'20px'
             });
-            if ((danmu.offset().left + danmu.width()) < $('.jumbotron').offset().left) {
-                danmu.remove();
-                clearInterval(timer);
-            }
-        }, 10);
+      var time = 10000 + 10000 * Math.random();
+      obj.animate({right:'+=1500'},time,'linear',function(){
+         obj.remove();
+      });
 
-     };
+      }
+
+  var runDanMu = function(){
+          if(arr.length > 0){
+                var n = Math.floor(Math.random()* arr.length);
+                var danmu = $("<div class='danmu'>"+arr[n]+"</div>");
+                $(".jumbotron").append(danmu);
+                 //console.log("loop:"+danmu.html());
+                 move(danmu);
+             }
+          setTimeout(runDanMu,3000);
+       }
+       runDanMu();
    });
